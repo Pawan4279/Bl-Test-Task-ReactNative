097d510da079ee56bc73e0e29f62636de496c80c
Pawan4279 <ps3642318@gmail.com>
2024-07-21 14:00:45 +0530

    Feature: add navigation controll.

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeScreen = (props) =>{
+
+    return(
+        <View style={{height:200,width:200,borderWidth:1}}><Text>Home Screen</Text></View>
+    )
+}
+
+export default HomeScreen;
