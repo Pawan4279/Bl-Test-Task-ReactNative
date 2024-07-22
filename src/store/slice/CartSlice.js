3b5ea70065c78dd55d9d55f7c5ea85a22a92b7aa
Pawan4279 <ps3642318@gmail.com>
2024-07-22 12:30:52 +0530

    a

---
+++ src/store/slice/CartSlice.js
@@ -0,0 +1,33 @@
+// slices/cartSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const item = state.items.find(item => item.id === action.payload.id);
+      if (item) {
+        item.quantity += 1;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeItem: (state, action) => {
+      const item = state.items.find(item => item.id === action.payload.id);
+      if (item.quantity > 1) {
+        item.quantity -= 1;
+      } else {
+        state.items = state.items.filter(item => item.id !== action.payload.id);
+      }
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
